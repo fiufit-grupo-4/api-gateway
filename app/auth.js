d7462df03a2dfd0518b26e8c8b47e4a6f9977f7c
Federico Pacheco <fpacheco@fi.uba.ar>
2023-04-12 03:24:37 -0300

    jwt updated

--- app/auth.js
+++ app/auth.js
@@ -1,5 +1,7 @@
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
+const moment = require("moment");
+const validator = require('validator');
 
 dotenv.config();
 
@@ -19,10 +21,26 @@ const setupAuth = (app, routes) => {
 
                 try {
                     const decoded = jwt.verify(token, process.env.JWT_SECRET); // TODO: complete JWT_SECRET at .env
+                    if (!decoded.exp || decoded.exp <= moment().unix() || !decoded.iat || decoded.iat > moment().unix()) {
+                        return res.status(401).json({ error: 'Without (or invalid) exp/iat time'})
+                    }
+
+                    if (!decoded._id || !decoded.mail) {
+                        return res.status(401).json({ error: 'Missing user information in token' });
+                    }
+
+                    if (!validator.isEmail(decoded.mail)) {
+                        return res.status(401).json({ error: 'Invalid mail in token' });
+                    }
+
+                    if (!validator.isMongoId(decoded._id)) {
+                        return res.status(401).json({ error: 'Invalid id in token' });
+                    }
+
                     req.user = decoded; // "decoded" has data included when the token was generated
                     next();
-                } catch (error) {
-                    return res.status(401).json({ error: 'Invalid token' });
+                } catch (err) {
+                    return res.status(401).json({ error: 'JWT Verify with error: ' + err });
                 }
             });
         }
@@ -30,3 +48,28 @@ const setupAuth = (app, routes) => {
 }
 
 exports.setupAuth = setupAuth;
+
+// la biblioteca JWT se asegura de que el token sea auténtico y no haya sido modificado, y 
+// también decodifica el token para obtener la información almacenada en el payload. 
+// Si el token es auténtico y no ha sido modificado, la biblioteca devolverá el payload del 
+// token como un objeto. 
+
+// Es responsabilidad del servidor validar y verificar si el payload es válido y corresponde
+// a un usuario registrado en el servidor pero es algo COSTOSO si desde el API GATEWAY estamos
+// consultando a la base de datos del user-microservice  a cada rato para validar el payload con 
+// un usuario registrado.
+
+
+// Podrías hacer una validación adicional en el servidor para verificar que los datos en el payload 
+// del token son válidos y corresponden a un usuario registrado en tu base de datos. 
+/// Por ejemplo, podrías agregar un campo en el payload que corresponda al ID del usuario y
+// luego verificar si ese ID existe en tu base de datos antes de otorgar acceso al usuario.. PERO ESO ES COSTO!!
+
+// También podrías agregar otros datos relevantes como roles o permisos y 
+// validarlos en el servidor para garantizar que el usuario tiene los permisos
+// adecuados para acceder a la información o realizar acciones específicas. 
+
+
+// En general, es importante tener en cuenta que JWT solo garantiza la integridad y autenticidad del token, 
+// pero no garantiza que los datos dentro del token sean válidos o estén autorizados para realizar 
+// acciones específicas en tu servidor.
